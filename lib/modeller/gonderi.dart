9123ace638ebeba5b922c52d7a201b2336159fef
Superknowa Courses <59803143+superknowa@users.noreply.github.com>
2020-02-29 17:29:41 +0300

    Gonderi Modeli Olusturuldu

---
+++ lib/modeller/gonderi.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Gonderi {
+
+  final String id;
+  final String gonderResimiUrl;
+  final String aciklama;
+  final String yayinlayanId;
+  final String kullaniciAdi;
+  final String begeniSayisi;
+  final String konum;
+  final Timestamp timestamp;
+
+  Gonderi({this.id, this.gonderResimiUrl, this.aciklama, this.yayinlayanId, this.kullaniciAdi, this.begeniSayisi, this.konum, this.timestamp});
+
+  factory Gonderi.dokumandanUret(DocumentSnapshot doc) {
+    return Gonderi(
+      id : doc['id'],
+      gonderResimiUrl: doc['gonderResimiUrl'],
+      aciklama: doc['aciklama'],
+      yayinlayanId: doc['yayinlayanId'],
+      kullaniciAdi: doc['kullaniciAdi'],
+      begeniSayisi: doc['begeniSayisi'],
+      konum: doc['konum'],
+      timestamp: doc['timestamp'],
+    );
+  }
+
+
+}
