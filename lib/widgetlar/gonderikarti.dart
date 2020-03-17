3d4d048cc1f60cf7eb55f5acb6876f5fac16ad74
Superknowa Courses <59803143+superknowa@users.noreply.github.com>
2020-03-17 14:44:26 +0300

    Some fix on gonderi kartı

--- lib/widgetlar/gonderikarti.dart
+++ lib/widgetlar/gonderikarti.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:socialapp/modeller/gonderi.dart';
 import 'package:socialapp/modeller/kullanici.dart';
+import 'package:socialapp/sayfalar/profil.dart';
 import 'package:socialapp/sayfalar/yorumlar.dart';
 import 'package:socialapp/servisler/firestoreservisi.dart';
 import 'package:socialapp/servisler/yetkilendirmeservisi.dart';
@@ -13,86 +14,103 @@ class GonderiKart extends StatefulWidget {
 
   const GonderiKart({this.gonderi, this.yayinlayan});
 
-
   @override
   _GonderiKartState createState() => _GonderiKartState();
 }
 
 class _GonderiKartState extends State<GonderiKart> {
-
   int _begeniSayisi = 0;
   bool _begendin = false;
   String aktifKullaniciId;
 
   @override
-  void initState() { 
-
+  void initState() {
     super.initState();
-    aktifKullaniciId = Provider.of<YetkilendirmeServisi>(context, listen: false).aktifKullaniciId;
+    aktifKullaniciId = Provider.of<YetkilendirmeServisi>(context, listen: false)
+        .aktifKullaniciId;
     _begeniSayisi = widget.gonderi.begeniSayisi;
     begeniVarMi();
-    
   }
 
   begeniVarMi() async {
-    bool begeniVarMi = await FireStoreServisi().begeniVarmi(aktifKullaniciId: aktifKullaniciId,gonderi: widget.gonderi);
-    if(begeniVarMi){
-
-      if (mounted) { 
+    bool begeniVarMi = await FireStoreServisi().begeniVarmi(
+        aktifKullaniciId: aktifKullaniciId, gonderi: widget.gonderi);
+    if (begeniVarMi) {
+      if (mounted) {
         setState(() {
           _begendin = true;
         });
       }
-
     }
   }
 
-  gonderiSecenekleri(){
-
+  gonderiSecenekleri() {
     return showDialog(
-      context: context,
-      builder: (context){
-        return SimpleDialog(
-          title: Text("Seçiminizi nedir?"),
-          children: <Widget>[
-            SimpleDialogOption(
-               child: Text("Gönderiyi Sil"),
-               onPressed: (){
-                 FireStoreServisi().gonderiSil(aktifKullaniciId: aktifKullaniciId,gonderi: widget.gonderi);
-                 Navigator.pop(context);
-               },
-            ),
-             SimpleDialogOption(
-               child: Text("Vazgeç",style: TextStyle(color: Colors.red)),
-               onPressed: (){
-                 Navigator.pop(context);
-               },
-            )
-          ],
-        );
-      }
-      );
-
-
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: Text("Seçiminiz nedir?"),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: Text("Gönderiyi Sil"),
+                onPressed: () {
+                  FireStoreServisi().gonderiSil(
+                      aktifKullaniciId: aktifKullaniciId,
+                      gonderi: widget.gonderi);
+                  Navigator.pop(context);
+                },
+              ),
+              SimpleDialogOption(
+                child: Text("Vazgeç", style: TextStyle(color: Colors.red)),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
   }
 
   gonderiBasligi() {
     return ListTile(
       leading: Padding(
-        padding: const EdgeInsets.only(left:12.0),
-        child: CircleAvatar(
-          backgroundColor: Colors.blue,
-          backgroundImage: CachedNetworkImageProvider(widget.yayinlayan.fotoUrl),
+        padding: const EdgeInsets.only(left: 12.0),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Profil(
+                          profilSahibiId: widget.gonderi.yayinlayanId,
+                          aktifKullaniciId: aktifKullaniciId,
+                        )));
+          },
+          child: CircleAvatar(
+            backgroundColor: Colors.blue,
+            backgroundImage:
+                CachedNetworkImageProvider(widget.yayinlayan.fotoUrl),
+          ),
         ),
       ),
-      title: Text(
-        widget.yayinlayan.kullaniciAdi,
-        style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+      title: GestureDetector(
+        onTap: (){
+          Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Profil(
+                          profilSahibiId: widget.gonderi.yayinlayanId,
+                          aktifKullaniciId: aktifKullaniciId,
+                        )));
+        },
+              child: Text(
+          widget.yayinlayan.kullaniciAdi,
+          style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+        ),
       ),
-      trailing: IconButton(
+      trailing: aktifKullaniciId == widget.gonderi.yayinlayanId ? IconButton(
         icon: Icon(Icons.more_vert),
         onPressed: gonderiSecenekleri,
-      ),
+      ):null,
       contentPadding: EdgeInsets.all(0.0),
     );
   }
@@ -100,13 +118,15 @@ class _GonderiKartState extends State<GonderiKart> {
   gonderiResmi() {
     return GestureDetector(
       onDoubleTap: begeniDegistir,
-          child: Container(
+      child: Container(
         height: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: CachedNetworkImageProvider(widget.gonderi.gonderResimiUrl,
-            ),fit: BoxFit.cover,)
-        ),
+            image: DecorationImage(
+          image: CachedNetworkImageProvider(
+            widget.gonderi.gonderResimiUrl,
+          ),
+          fit: BoxFit.cover,
+        )),
       ),
     );
   }
@@ -120,14 +140,16 @@ class _GonderiKartState extends State<GonderiKart> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             IconButton(
-              icon: _begendin ? Icon(
-                Icons.favorite,
-                color: Colors.red,
-                size: 35.0,
-              ) :  Icon(
-                Icons.favorite_border,
-                size: 35.0,
-              ),
+              icon: _begendin
+                  ? Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                      size: 35.0,
+                    )
+                  : Icon(
+                      Icons.favorite_border,
+                      size: 35.0,
+                    ),
               onPressed: begeniDegistir,
             ),
             IconButton(
@@ -149,50 +171,57 @@ class _GonderiKartState extends State<GonderiKart> {
         SizedBox(
           height: 2.0,
         ),
-        widget.gonderi.aciklama.isNotEmpty ? Row(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 8.0,
-                right: 8.0,
-              ),
-              child: Text(
-                widget.yayinlayan.kullaniciAdi,
-                style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Expanded(child: Text(widget.gonderi.aciklama))
-          ],
-        ) : SizedBox(height: 0.0,)
+        widget.gonderi.aciklama.isNotEmpty
+            ? Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 8.0,
+                      right: 8.0,
+                    ),
+                    child: Text(
+                      widget.yayinlayan.kullaniciAdi,
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Expanded(child: Text(widget.gonderi.aciklama))
+                ],
+              )
+            : SizedBox(
+                height: 0.0,
+              )
       ],
     );
   }
 
-  yorumlaraGit(){
-    Navigator.push(context, MaterialPageRoute(builder: (context)=>Yorumlar(gonderi: widget.gonderi,)));
+  yorumlaraGit() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => Yorumlar(
+                  gonderi: widget.gonderi,
+                )));
   }
 
-  begeniDegistir(){
-
-
-    if(_begendin){
+  begeniDegistir() {
+    if (_begendin) {
       //Beğenmiş durumdasın, beğeniden çıkart
       setState(() {
         _begendin = false;
-        _begeniSayisi = _begeniSayisi  - 1;
+        _begeniSayisi = _begeniSayisi - 1;
       });
 
-      FireStoreServisi().gonderiBegeniKaldir(aktifKullaniciId:aktifKullaniciId , gonderi:widget.gonderi);
-      
-
+      FireStoreServisi().gonderiBegeniKaldir(
+          aktifKullaniciId: aktifKullaniciId, gonderi: widget.gonderi);
     } else {
-      
       //Henüz beğenmemişsin, beğen
       setState(() {
         _begendin = true;
-        _begeniSayisi = _begeniSayisi  + 1;
+        _begeniSayisi = _begeniSayisi + 1;
       });
-      FireStoreServisi().gonderiBegen(aktifKullaniciId:aktifKullaniciId , gonderi:widget.gonderi);
+      FireStoreServisi().gonderiBegen(
+          aktifKullaniciId: aktifKullaniciId, gonderi: widget.gonderi);
     }
   }
 
