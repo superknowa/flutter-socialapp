434aa77dd6418ab7fd62588f0ae36b06013544cb
Superknowa Courses <59803143+superknowa@users.noreply.github.com>
2020-10-04 14:43:58 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,127 +2,90 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+exports.takipGerceklesti = functions.firestore.document('takipciler/{takipEdilenId}/kullanicininTakipcileri/{takipEdenKullaniciId}').onCreate(async (snapshot, context) => {
+    const takipEdilenId = context.params.takipEdilenId;
+    const takipEdenId = context.params.takipEdenKullaniciId;
 
+   const gonderilerSnapshot = await admin.firestore().collection("gonderiler").doc(takipEdilenId).collection("kullaniciGonderileri").get();
 
-exports.takipGerceklesti = functions.firestore
-  .document("takipciler/{kullaniciId}/kullanicininTakipcileri/{takipciId}")
-  .onCreate(async (snapshot, context) => {
+   gonderilerSnapshot.forEach((doc)=>{
+        if(doc.exists){
+            const gonderiId = doc.id;
+            const gonderiData = doc.data();
 
-    const kullaniciId = context.params.kullaniciId;
-    const takipciId = context.params.takipciId;
+            admin.firestore().collection("akislar").doc(takipEdenId).collection("kullaniciAkisGonderileri").doc(gonderiId).set(gonderiData);
+        }
+   });
+});
 
-    //Takip edilen kullanıcının gönderilerini getirelim
-    const gonderilerSnapshot = await admin.firestore().collection("gonderiler").doc(kullaniciId).collection("kullaniciGonderileri").get();
+exports.takiptenCikildi = functions.firestore.document('takipciler/{takipEdilenId}/kullanicininTakipcileri/{takipEdenKullaniciId}').onDelete(async (snapshot, context) => {
+    const takipEdilenId = context.params.takipEdilenId;
+    const takipEdenId = context.params.takipEdenKullaniciId;
 
-    //Burda kullandığımız dil javascript olduğu için yazım farklılıkları var. Kafanızı karıştırmasın. 
-    //foeach metodu ile her bir dökümana tek tek ulaşıcam ve takip edenin akış koleksiyonuna eklicem.
-    gonderilerSnapshot.forEach(doc => {
+   const gonderilerSnapshot = await admin.firestore().collection("akislar").doc(takipEdenId).collection("kullaniciAkisGonderileri").where("yayinlayanId", "==", takipEdilenId).get();
 
-      if (doc.exists) {
-        const gonderiId = doc.id;
-        const gonderiData = doc.data();
-        admin.firestore().collection("akis").doc(takipciId).collection("kullaniciAkis").doc(gonderiId).set(gonderiData);
-      }
+   gonderilerSnapshot.forEach((doc)=>{
+        if(doc.exists){
+            doc.ref.delete();
+        }
+   });
+});
 
-    });
-
-
-  });
-
-
-exports.takiptenCikildi = functions.firestore
-  .document("takipciler/{kullaniciId}/kullanicininTakipcileri/{takipciId}")
-  .onDelete(async (snapshot, context) => {
-
-    const kullaniciId = context.params.kullaniciId;
-    const takipciId = context.params.takipciId;
-
-    //
-    const takiptEdilenGonderilerSnapshot = await admin.firestore().collection("akis").doc(takipciId).collection("kullaniciAkis").where("yayinlayanId", "==", kullaniciId).get();
-
-    //foeach metodu ile her bir dökümana tek tek ulaşıcam ve takip edenin akış koleksiyonuna eklicem.
-    takiptEdilenGonderilerSnapshot.forEach(doc => {
-
-      if (doc.exists) {
-        doc.ref.delete();
-      }
-
-    });
-
-
-  });
-
-
-  exports.yeniGonderiEklendi = functions.firestore
-  .document("gonderiler/{kullaniciId}/kullaniciGonderileri/{gonderiId}")
-  .onCreate(async (snapshot, context) => {
-
-    //Adresteki bilgileri değişkenlere aldık.
-    const kullaniciId = context.params.kullaniciId;
+exports.yeniGonderiEklendi = functions.firestore.document('gonderiler/{takipEdilenKullaniciId}/kullaniciGonderileri/{gonderiId}').onCreate(async (snapshot, context) => {
+    const takipEdilenId = context.params.takipEdilenKullaniciId;
     const gonderiId = context.params.gonderiId;
-    const eklenenKayit = snapshot.data();
-    
-    const takipcilerSnapshot = await admin.firestore().collection("takipciler").doc(kullaniciId).collection("kullanicininTakipcileri").get();
+    const yeniGonderiData = snapshot.data();
+
+    const takipcilerSnapshot = await admin.firestore().collection("takipciler").doc(takipEdilenId).collection("kullanicininTakipcileri").get();
 
     takipcilerSnapshot.forEach(doc=>{
-      const takipciId = doc.id;
-      admin.firestore().collection("akis").doc(takipciId).collection("kullaniciAkis").doc(gonderiId).set(eklenenKayit);
-
+        const takipciId = doc.id;
+        admin.firestore().collection("akislar").doc(takipciId).collection("kullaniciAkisGonderileri").doc(gonderiId).set(yeniGonderiData);
     });
+});
 
-
-  });
-
-
-  exports.gonderiGuncellendi = functions.firestore
-  .document("gonderiler/{kullaniciId}/kullaniciGonderileri/{gonderiId}")
-  .onUpdate(async (snapshot, context) => {
-
-    //Adresteki bilgileri değişkenlere aldık.
-    const kullaniciId = context.params.kullaniciId;
+exports.gonderiGuncellendi = functions.firestore.document('gonderiler/{takipEdilenKullaniciId}/kullaniciGonderileri/{gonderiId}').onUpdate(async (snapshot, context) => {
+    const takipEdilenId = context.params.takipEdilenKullaniciId;
     const gonderiId = context.params.gonderiId;
-    const guncellenmisGonderi = snapshot.after.data();
-    
+    const guncellenmisGonderiData = snapshot.after.data();
 
-    //console.log("Açıklama: " + guncellenmisGonderi.aciklama);
-    //const guncellenmemisGonderi = snapshot.before.data();
-    
-    
-    const takipcilerSnapshot = await admin.firestore().collection("takipciler").doc(kullaniciId).collection("kullanicininTakipcileri").get();
+    const takipcilerSnapshot = await admin.firestore().collection("takipciler").doc(takipEdilenId).collection("kullanicininTakipcileri").get();
 
-    takipcilerSnapshot.forEach(async takipciDoc=>{
-      const takipciId = takipciDoc.id;
-      const akisDoc = await admin.firestore().collection("akis").doc(takipciId).collection("kullaniciAkis").doc(gonderiId).get();
-      if(akisDoc.exists){
-        akisDoc.ref.update(guncellenmisGonderi);
-      }
-
+    takipcilerSnapshot.forEach(doc=>{
+        const takipciId = doc.id;
+        admin.firestore().collection("akislar").doc(takipciId).collection("kullaniciAkisGonderileri").doc(gonderiId).update(guncellenmisGonderiData);
     });
+});
 
-
-  });
-
-
-  exports.gonderiSilindi= functions.firestore
-  .document("gonderiler/{kullaniciId}/kullaniciGonderileri/{gonderiId}")
-  .onDelete(async (snapshot, context) => {
-
-    
-    const kullaniciId = context.params.kullaniciId;
+exports.gonderiSilindi = functions.firestore.document('gonderiler/{takipEdilenKullaniciId}/kullaniciGonderileri/{gonderiId}').onDelete(async (snapshot, context) => {
+    const takipEdilenId = context.params.takipEdilenKullaniciId;
     const gonderiId = context.params.gonderiId;
-    
-    
-    const takipcilerSnapshot = await admin.firestore().collection("takipciler").doc(kullaniciId).collection("kullanicininTakipcileri").get();
 
-    takipcilerSnapshot.forEach(async takipciDoc=>{
-      const takipciId = takipciDoc.id;
-      const akisDoc = await admin.firestore().collection("akis").doc(takipciId).collection("kullaniciAkis").doc(gonderiId).get();
-      if(akisDoc.exists){
-        akisDoc.ref.delete();
-      }
+    const takipcilerSnapshot = await admin.firestore().collection("takipciler").doc(takipEdilenId).collection("kullanicininTakipcileri").get();
 
+    takipcilerSnapshot.forEach(doc=>{
+        const takipciId = doc.id;
+        admin.firestore().collection("akislar").doc(takipciId).collection("kullaniciAkisGonderileri").doc(gonderiId).delete();
     });
+});
 
+/*
+exports.kayitSilindi = functions.firestore.document('deneme/{docId}').onDelete((snapshot, context) => {
+    admin.firestore().collection("gunluk").add({
+        "aciklama":"Deneme koleksiyonundan kayıt silindi."
+    });
+});
 
-  });
+exports.kayitGuncellendi = functions.firestore.document('deneme/{docId}').onUpdate((change, context) => {
+    admin.firestore().collection("gunluk").add({
+        "aciklama":"Deneme koleksiyonunda kayıt güncellendi."
+    });
+});
+
+exports.yazmaGerceklesti = functions.firestore.document('deneme/{docId}').onWrite((change, context) => {
+    admin.firestore().collection("gunluk").add({
+        "aciklama":"Deneme koleksiyonunda veri ekleme, silme, güncelleme işlemlerinden biri gerçekleşti."
+    });
+});
+*/
 
